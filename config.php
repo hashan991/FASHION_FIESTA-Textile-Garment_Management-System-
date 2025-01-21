5d9c9d1f8dc86513db9d21a6f57cef651cb1e513
Hashan Lahiru <hashanlahiru2068@gmail.com>
2025-01-21 11:44:58 +0530

    1111

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "RUWINIfeb13";
+$password = "hasa11";
 $database = "clothing";
 
-$conn = new mysqli($servername, $username, $password, $database,3306);
+$conn = new mysqli($servername, $username, $password, $database,3307);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
